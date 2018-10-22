4708eab4e9340fc86092f3e4920952946fe633db
reshke <rocklobster2@mail.ru>
2018-10-22 21:04:17 +0500

    something

--- src/main/scala/fintech/homework04/Tree.scala
+++ src/main/scala/fintech/homework04/Tree.scala
@@ -1,21 +1,70 @@
 package fintech.homework04
 
+import scala.annotation.tailrec
+
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
   // реализовать функцию fold
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+
+  private case class Node[B](current_depth : Int)(val value : B)
+
+  @tailrec
+  def fold_times[A,B](current_depth : Int)(acc : Seq[Node[B]])(seq: Seq[Tree[A]])(f: A => B)(g: (B,B) => B): B = {
+    var newAcc: Seq[Node[B]] = acc
+    var new_depth = current_depth
+
+    while (newAcc.length > 1 && newAcc.head.current_depth == newAcc.tail.head.current_depth) {
+      val head = newAcc.head
+      newAcc = newAcc.tail
+
+      val el = Node(head.current_depth - 1)(g(head.value, newAcc.head.value))
+      newAcc = el +: newAcc.tail
+      new_depth -= 1
+    }
+
+
+    if (seq.nonEmpty)
+      seq.head match {
+        case Leaf(value) => fold_times(new_depth)(Node(new_depth)(f(value)) +: newAcc)(seq.tail)(f)(g)
+        case Branch(left, right) => fold_times(new_depth + 1)(newAcc)(right +: left +: seq.tail)(f)(g)
+      }
+    else
+      newAcc.head.value
+  }
+
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = {
+    val seq: Seq[Tree[A]] = Seq(t)
+    val acc: Seq[Node[B]] = Seq.empty[Node[B]]
+    fold_times(0)(acc)(seq)(f)(g)
+    //    t match {
+    //      case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+    //      case Leaf(value) => f(value) // это без tailrec, зато работает и всего 2 строки
+    //    }
+  }
 
   // реализовать следующие функции через fold
 
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = {
+    fold[A, Int](t)((_: A) => 1)((x: Int, y  :Int) => x + y + 1)
+  }
 
-  def max(t: Tree[Int]): Int = ???
+  def max(t: Tree[Int]): Int = {
+    fold[Int, Int](t)((x: Int) => x)((x: Int, y : Int) => math max(x, y))
+  }
 
-  def depth[A](t: Tree[A]): Int = ???
+  def depth[A](t: Tree[A]): Int = {
+    fold[A, Int](t)((_ : A) => 0)((x : Int, y: Int) => math.max(x, y) + 1)
+  }
 
   //здесь возможно придется прибегнуть к насильному указанию типа: Leaf(a): Tree[A]
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = {
+    val g = (x : Tree[B], y: Tree[B]) => new Branch[B](x, y)
+    fold[A, Tree[B]](t)((x : A) => Leaf(f(x)))(g)
+  }
 }
+
+
+
--- src/test/scala/fintech/homework04/TreeSpec.scala
+++ src/test/scala/fintech/homework04/TreeSpec.scala
@@ -1,6 +1,94 @@
+
+
 package fintech.homework04
 import org.scalatest.{FlatSpec, Matchers}
+import Tree.max, Tree.depth, Tree.map
 
 class TreeSpec extends FlatSpec with Matchers {
-  ???
+
+  val extraLittleTree : Leaf[Int] = Leaf[Int](1)
+  val littleTree : Branch[Int] = Branch[Int](Leaf[Int](7), Leaf[Int](8))
+  val biggerTree : Branch[Int] = Branch[Int](Leaf[Int](2), littleTree)
+  val binaryTree : Branch[Int] = Branch[Int](Branch[Int](Leaf(2), Leaf(3)), Branch[Int](Leaf(71), Leaf(8)))
+  val tree : Branch[Int] = Branch[Int](Branch[Int](Leaf[Int](9), Leaf[Int](1)),
+    Branch[Int](littleTree, biggerTree))
+  val bigTree : Branch[Int] = Branch[Int](binaryTree, tree)
+
+
+  it should "work well with max function" in {
+    max(extraLittleTree) should be(1)
+    max(littleTree) should be(8)
+    max(biggerTree) should be(8)
+    max(tree) should be(9)
+    max(binaryTree) should be(71)
+    max(bigTree) should be(71)
+  }
+
+  it should "work well with size function" in {
+    Tree.size(extraLittleTree) should be(1)
+    Tree.size(littleTree) should be(3)
+    Tree.size(biggerTree) should be(5)
+    Tree.size(tree) should be(13)
+    Tree.size(binaryTree) should be(7)
+    Tree.size(bigTree) should be(21)
+  }
+
+  it should "work well with size function and not depend on the type" in {
+    val littleTree: Branch[Seq[Char]] = Branch[Seq[Char]](Leaf[Seq[Char]](List('a', 'o')),
+      Leaf[Seq[Char]](List('y', 'i')))
+
+    val biggerTree: Branch[Any] = Branch[Any](Leaf[String]("2uwihi"), littleTree)
+    val tree : Branch[Any] = Branch[Any](Branch[Any](Leaf[Any](9), Leaf[Any](1)),
+      Branch[Any](littleTree, biggerTree))
+
+    Tree.size(littleTree) should be(3)
+    Tree.size(biggerTree) should be(5)
+    Tree.size(tree) should be(13)
+  }
+
+  it should "work well with depth function" in {
+    depth(extraLittleTree) should be(0)
+    depth(littleTree) should be(1)
+    depth(biggerTree) should be(2)
+    depth(tree) should be(4)
+    depth(binaryTree) should be(2)
+    depth(bigTree) should be(5)
+  }
+
+  it should "work well with depth function and not depend on the type" in {
+    val littleTree: Branch[Seq[Char]] = Branch[Seq[Char]](Leaf[Seq[Char]](List('a', 'o')),
+      Leaf[Seq[Char]](List('y', 'i')))
+
+    val biggerTree: Branch[Any] = Branch[Any](Leaf[String]("2uwihi"), littleTree)
+    val tree : Branch[Any] = Branch[Any](Branch[Any](Leaf[Any](9), Leaf[Any](1)),
+      Branch[Any](littleTree, biggerTree))
+
+    depth(littleTree) should be(1)
+    depth(biggerTree) should be(2)
+    depth(tree) should be(4)
+  }
+
+  it should "map trees well" in {
+    val withDoubleLittleTree = map(littleTree)((x : Int) => x.toDouble)
+
+    withDoubleLittleTree match {
+      case Branch(left, right) => left should be(Leaf(7.0)); right should be(Leaf(8.0))
+      case _ => throw new AssertionError()
+    }
+
+    val withDoubleBigTree = map(bigTree)((x : Int) => x.toDouble)
+
+    withDoubleBigTree match {
+      case Branch(left, right) => left should be(
+        Branch[Double](Branch[Double](Leaf(2.0), Leaf(3.0)), Branch[Double](Leaf(71.0), Leaf(8.0))))
+
+        val littleDoubleTree = Branch[Double](Leaf[Double](7), Leaf[Double](8))
+
+        right should be(Branch[Double](Branch[Double](Leaf[Double](9), Leaf[Double](1)),
+          Branch[Double](littleDoubleTree,
+            Branch[Double](Leaf[Double](2), littleDoubleTree))))
+
+      case _ => throw new AssertionError("something goes wrong!")
+    }
+  }
 }
